08a635792ccd271522fb4e9e9c178a3f1f3f7a37
wzl <truthwzl@sina.com>
2022-08-06 16:56:51 +0800

    init all.

---
+++ demo-consumer/src/main/java/com/dubbo/example/consumer/DubboConsumerBootstrap.java
@@ -0,0 +1,45 @@
+package com.dubbo.example.consumer;
+
+import com.dubbo.example.DemoService;
+
+import org.apache.dubbo.config.annotation.Reference;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@EnableAutoConfiguration
+public class DubboConsumerBootstrap {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @Reference(version = "1.0.0")
+
+    private DemoService demoService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerBootstrap.class).close();
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        final String result = "";
+        int n = 1000;
+        do {
+            n--;
+            String result2 = demoService.sayHello("wangzhenli"+n);
+            logger.info(result2);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }while(n>0);
+
+        return args -> logger.info(result);
+    }
+}
---
+++ demo-interface/src/main/java/com/dubbo/example/DemoService.java
@@ -0,0 +1,7 @@
+package com.dubbo.example;
+
+public interface DemoService {
+
+    String sayHello(String name);
+    int add(int a, int b);
+}
